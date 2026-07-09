0711fcc6e78d9de2461d7aaddb5078d94c9d04ad
colcamenterprises-collab <colcamenterprises@gmail.com>
2026-07-09 12:41:46 +0700

    Load hero and services layout fixes

--- src/main.tsx
+++ src/main.tsx
@@ -8,6 +8,7 @@ import './expandingMedia.css';
 import './finalPolish.css';
 import './logoPolish.css';
 import './processHover.css';
+import './heroServicesLayout.css';
 import { App } from './App';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
